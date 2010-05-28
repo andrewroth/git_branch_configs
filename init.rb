a4eba76e7882b38721ddf113c9cdf014cf66a3cc
Andrew Roth <andrewroth@gmail.com>
2010-05-28 17:01:48 -0400

    make loading more robust

--- init.rb
+++ init.rb
@@ -5,28 +5,50 @@ CONFIGS_PATH = "config/branch_configs"
 MAPPINGS_FILE = "#{CONFIGS_PATH}/branch_mappings.yml"
 
 if File.exists? MAPPINGS_FILE
-  # TODO: get current git branch
   mappings = YAML::load(File.read(MAPPINGS_FILE))
   
-  if mappings.class == Hash
+  if mappings.class == Array
+
+    g = Git.init
+    git_branch = g.lib.branch_current
+
+    # special case to handle specific checkout of an sha
+    head = g.object('HEAD').sha
+    if git_branch == "(no branch)"
+      for branch in (g.branches.remote + g.branches.local)
+        if branch.gcommit.sha == head
+          git_branch = branch.name
+          break
+        end
+      end
+    end
 
     for key_path in mappings
       key = key_path.keys.first
       path = key_path.values.first
       key =~ /\/(.*)\//
-        if $1 && Regexp.new($1).match(git_branch)
-          config = path
-          break
-        elsif key == git_branch
-          config = path
-        end
+      if $1 && Regexp.new($1).match(git_branch)
+        config_path = path
+        break
+      elsif key == git_branch
+        config_path = path
+      end
     end
 
-    puts "Git Branch Config: #{config}" if config
+    if config_path
+      puts "Git Branch Config: #{git_branch} -> #{full_config_path}"
+      full_config_path = "#{CONFIGS_PATH}/#{config_path}"
+    else
+      puts "Git Branch Config: no match"
+    end
 
-    unless File.exists?(config)
+    if config_path && File.exists?(full_config_path)
+      require full_config_path
+    elsif config_path
       puts "Git Branch Config: config file specified does not exist"
     end
   end
 
 end
+
+true
