eaac1106e0ba38a73776a387784776791e6d31a5
Andrew Roth <andrewroth@gmail.com>
2010-05-28 16:15:19 -0400

    make generator work; handle empty yml file

--- generators/git_branch_configs/git_branch_configs_generator.rb
+++ generators/git_branch_configs/git_branch_configs_generator.rb
@@ -2,7 +2,7 @@ class GitBranchConfigsGenerator < Rails::Generator::Base
   def manifest
     record do |m|
       m.directory "config/branch_configs"
-      m.file "config_mappings.yml", "config/branch_configs/branch_mappings.yml"
+      m.file "branch_mappings.yml", "config/branch_configs/branch_mappings.yml"
     end
   end
 end
--- init.rb
+++ init.rb
@@ -7,23 +7,26 @@ MAPPINGS_FILE = "#{CONFIGS_PATH}/branch_mappings.yml"
 if File.exists? MAPPINGS_FILE
   # TODO: get current git branch
   mappings = YAML::load(File.read(MAPPINGS_FILE))
+  
+  if mappings.class == Hash
 
-  for key_path in mappings
-    key = key_path.keys.first
-    path = key_path.values.first
-    key =~ /\/(.*)\//
-    if $1 && Regexp.new($1).match(git_branch)
-      config = path
-      break
-    elsif key == git_branch
-      config = path
+    for key_path in mappings
+      key = key_path.keys.first
+      path = key_path.values.first
+      key =~ /\/(.*)\//
+        if $1 && Regexp.new($1).match(git_branch)
+          config = path
+          break
+        elsif key == git_branch
+          config = path
+        end
+    end
+
+    puts "Git Branch Config: #{config}" if config
+
+    unless File.exists?(config)
+      puts "Git Branch Config: config file specified does not exist"
     end
   end
 
-  puts "Git Branch Config: #{config}" if config
-
-  unless File.exists?(config)
-    puts "Git Branch Config: config file specified does not exist"
-  end
 end
-
