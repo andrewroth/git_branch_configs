979624b5adb70f7c1c0cc0a2f147359be8a60e7c
Andrew Roth <andrewroth@gmail.com>
2010-05-29 11:26:00 -0400

    make generator add database.common.yml

--- generators/git_branch_configs/git_branch_configs_generator.rb
+++ generators/git_branch_configs/git_branch_configs_generator.rb
@@ -3,6 +3,7 @@ class GitBranchConfigsGenerator < Rails::Generator::Base
     record do |m|
       m.directory "config/branch_configs"
       m.file "branch_mappings.yml", "config/branch_configs/branch_mappings.yml"
+      m.file "database.common.yml", "config/database.common.yml"
     end
   end
 end
